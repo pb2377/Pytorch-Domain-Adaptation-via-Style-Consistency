a125c92ab705551e826b7c3b8b695aa90caea04a
pb652 <p.boulton@bath.ac.uk>
2021-03-01 15:09:46 +0000

    first commit

---
+++ models/layers/modules/__init__.py
@@ -0,0 +1,5 @@
+from .l2norm import L2Norm
+from .multibox_loss import MultiBoxLoss, SelfMultiBoxLoss
+from .feature_loss import FeatureConsistency
+
+__all__ = ['L2Norm', 'MultiBoxLoss']
---
+++ models/layers/modules/feature_loss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+
+
+class FeatureConsistency(nn.Module):
+    def __init__(self, cosine=False):
+        super(FeatureConsistency, self).__init__()
+        self.cosine = cosine
+        if cosine:
+            self.criterion = nn.CosineEmbeddingLoss(reduction='none')
+        else:
+            self.criterion = torch.nn.MSELoss(reduction='none')
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)
+
+    def forward(self, x1, x2):
+        if self.cosine:
+            return self.criterion(x1, x2, target=torch.ones(1)).mean()
+        else:
+            return self.criterion(x1, x2).mean()
